bbc0b9c61d75c8d3705a9c3667d332d1157deb07
Richard Mbingi <mbingicoding@gmail.com>
2024-06-22 20:06:44 +0300

    Again we start from the beninnging 😂😂😂

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import { Text } from "react-native";
+
+function App() {
+  return <Text>This is a test</Text>;
+}
+
+export default App;
